d029464c147273cffed69fc2fe6c9683f6b497fd
Amir <amkad87@gmail.com>
2025-12-05 01:22:40 +0400

    versions update

--- jcross_compose_prototype/app/build.gradle.kts
+++ jcross_compose_prototype/app/build.gradle.kts
@@ -1,17 +1,38 @@
+import java.util.Properties
+import java.io.FileInputStream
+
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
     id("org.jetbrains.kotlin.plugin.compose")
 }
 
+val keyStoreHome = System.getenv("ANDROID_KEYSTORE_HOME") // default directory for local development
+val keystoreFile = if (!keyStoreHome.isNullOrBlank()) "$keyStoreHome/keystore_2015.keystore" else System.getenv("ANDROID_KEYSTORE_KEY")
+val keystoreParamsFile = if (!keyStoreHome.isNullOrBlank()) "$keyStoreHome/keystore_2015.params" else System.getenv("ANDROID_KEYSTORE_PARAMS")
+
+val keystoreProperties = Properties().apply {
+    if (keystoreParamsFile != null) {
+        load(FileInputStream(keystoreParamsFile))
+    }
+}
+
 android {
     namespace = "com.jcross.prototype"
-    compileSdk = 35
+    compileSdk = 36
+    signingConfigs {
+        create("release") {
+            keyAlias = keystoreProperties["keyAlias"]?.toString()
+            keyPassword = keystoreProperties["keyPassword"]?.toString()
+            storeFile = if (keystoreFile != null) file(keystoreFile) else null
+            storePassword = keystoreProperties["storePassword"]?.toString()
+        }
+    }
 
     defaultConfig {
         applicationId = "com.jcross.prototype"
         minSdk = 24
-        targetSdk = 35
+        targetSdk = 36
         versionCode = 1
         versionName = "1.0"
 
@@ -23,6 +44,15 @@ android {
 
     buildTypes {
         release {
+            signingConfig = signingConfigs.getByName("release")
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+        debug {
+            signingConfig = signingConfigs.getByName("release")
             isMinifyEnabled = false
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
@@ -49,10 +79,10 @@ android {
 }
 
 dependencies {
-    implementation("androidx.core:core-ktx:1.12.0")
+    implementation("androidx.core:core-ktx:1.17.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.10.0")
     implementation("androidx.activity:activity-compose:1.12.1")
-    implementation(platform("androidx.compose:compose-bom:2025.11.01"))
+    implementation(platform("androidx.compose:compose-bom:2025.12.00"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
@@ -60,9 +90,9 @@ dependencies {
     implementation("androidx.navigation:navigation-compose:2.9.6")
 
     testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    androidTestImplementation(platform("androidx.compose:compose-bom:2025.11.01"))
+    androidTestImplementation("androidx.test.ext:junit:1.3.0")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.7.0")
+    androidTestImplementation(platform("androidx.compose:compose-bom:2025.12.00"))
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
